9d51ea75e3cf28a8019256764c2d129ef739c8d6
Arseny <arsuhinars@gmail.com>
2024-03-29 18:04:43 +0400

    Scripts updates

--- .scripts/swww-change-wallpaper.sh
+++ .scripts/swww-change-wallpaper.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 TRANSITIONS="-t fade
--t left
--t right
--t top
--t wipe
+-t wipe --transition-angle 45
+-t wipe --transition-angle 135
 -t any
 -t outer"
 
-RANDOM_WALLPAPER_PATH=$(find ~/Pictures/wallpapers/ -type f | sort -R | tail -n 1 -)
+RANDOM_WALLPAPER_PATH=$(find /home/arsuhinars/Pictures/wallpapers/ -type f | sort -R | tail -n 1 -)
 RANDOM_TRANSITION=$(echo "$TRANSITIONS" | sort -R | tail -n 1 -)
 
 swww img $RANDOM_WALLPAPER_PATH $RANDOM_TRANSITION --transition-fps=75
