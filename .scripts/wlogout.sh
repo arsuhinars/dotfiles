3a158a11b9f4b4efe911bdd4c73d623a516c6036
arsuhinars <arsuhinars@gmail.com>
2024-10-01 16:49:33 +0400

    wlogout fix

--- .scripts/wlogout.sh
+++ .scripts/wlogout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wlogout --buttons-per-row 2 -L 400px -R 400px -T 200px -B 200px -c 16px -r 16px
+pidof wlogout || wlogout --buttons-per-row 2 -L 400px -R 400px -T 200px -B 200px -c 16px -r 16px
